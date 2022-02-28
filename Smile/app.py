813b270440c6cdf2d99d000775ba0a0a5223ec1e
Steven18388 <18388@wc.school.nz>
2022-03-01 10:10:54 +1300

    new

--- Smile/app.py
+++ Smile/app.py
@@ -1,7 +1,20 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+DATABASE = "smile.db"
 
+def create_connection(db_file):
+    ###
+    Create a connection with the database
+    parameter:name of the database file
+    returns: a connection to the file
+    ###
+    try:
+        connection = sqlite3.connect(db_file)
+        return connection
+    excpet Error as e:
+        print(e)
+    return None
 
 @app.route('/')
 def render_homepage():
@@ -10,6 +23,13 @@ def render_homepage():
 
 @app.route('/menu')
 def render_menu_page():
+    con = create_connection(DATABASE)
+
+    query = "SELECT name, description, volume, price, image FROM product"
+    cur = con.cursor()
+    cur.execute(query)
+    product_list = cur.fetchall()
+
     producet_list = [["Flat white", "Minimal foam on a coffee", "250ml", 4.00],
                      ["Espresso", "A duble shot of coffee", "80ml", 3.00, "espresso"]]
     return render_template('menu.html')
